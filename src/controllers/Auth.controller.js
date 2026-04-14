4d90660774b8f060739f81e64da8f450442016d7
Ejaz47-Hub <ejaz47026@gmail.com>
2026-04-14 16:04:12 +0530

    Developed

--- src/controllers/Auth.controller.js
+++ src/controllers/Auth.controller.js
@@ -1,4 +1,5 @@
 import UserModel from "../models/user.model.js";
+import bcrypt from "bcryptjs"
 import jwt from "jsonwebtoken"
 async function registerUserController(req,res){
      const {username,password} = req.body
@@ -7,8 +8,9 @@ async function registerUserController(req,res){
     if(UserExists){
        return res.status(422).json({message:"User Already Registers"})
     }
+    const hash = await bcrypt.hash(password,10)
 
-    const CreateUser = await UserModel.create({username,password})
+    const CreateUser = await UserModel.create({username,password:hash})
     const token = jwt.sign({id:CreateUser._id},process.env.JWT_SECRET)
     res.cookie("token",token)
 
@@ -16,4 +18,25 @@ async function registerUserController(req,res){
    return res.status(201).json({message:"User Registerd",CreateUser:{user:CreateUser.username},token})
 
 }
-export default registerUserController
+
+async function loginUserController(req,res){
+    const{username,password} = req.body
+
+    const IsRegisterd = await UserModel.findOne({username,password})
+    if(!IsRegisterd){
+        return res.status(400).json({message:"User Not Registered"})
+    }
+
+  const result = await bcrypt.compare(password,IsRegisterd.password)
+  if(!result){
+    return res.status(400).json({message:"Entered Wrong Password"})
+  }
+
+  const token = jwt.sign({id:IsRegisterd._id},process.env.JWT_SECRET)
+  res.cookie("token",token)
+
+  res.status(200).json({message:"Login SuccessFully",user:{username},token})
+
+}
+
+export {registerUserController,loginUserController}
--- src/routes/Auth.Route.js
+++ src/routes/Auth.Route.js
@@ -1,7 +1,8 @@
-import registerUserController from "../controllers/Auth.controller.js";
+import{ registerUserController,loginUserController }from "../controllers/Auth.controller.js";
 import express from "express"
 
 const Router = express.Router()
 
 Router.post("/register",registerUserController)
+Router.post("/login",loginUserController)
 export default Router
