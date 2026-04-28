1e39a306649d61e4ade5ec174c919cc1ac3a1e6d
Ejaz47-Hub <ejaz47026@gmail.com>
2026-04-28 13:30:15 +0530

    Prac

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,6 +1,38 @@
 import React from 'react'
+import axiosInstance from '../api/axiosInstance.js'
+import { useEffect,useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 
 const Login = () => {
+  const navigate = useNavigate()
+  const [formData,SetFormData] = useState({username:"",password:""})
+  const [Error,Seterror] = useState("")
+  const [loading,SetLoading] = useState(false)
+
+  useEffect(()=>{
+    if (localStorage.getItem("token"))
+      navigate('/notes')
+  },[])
+
+  const handleChange = (e) =>{
+      SetFormData({...formData,[e.target.name]:e.target.value})
+  }
+
+  const handleSubmit = async(e) =>{
+    e.preventDefault()
+    SetLoading(true)
+    Seterror("")
+    try {
+      const response = await axiosInstance.post('/login', formData)
+      localStorage.getItem("token",response.data.token)
+      navigate('/notes')
+    } catch (error) {
+      Seterror(error.response?.data?.message || "Login Failed")
+    }finally{
+      SetLoading(false)
+    }
+  }
   return (
     <div>Login</div>
   )
